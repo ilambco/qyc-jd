c949b8300b26c863625470e99a34e54a370a9f51
ilambco <153483665+ilambco@users.noreply.github.com>
2025-11-14 22:36:27 +0800

    Add script to override window.open for image search

---
+++ js/searchImage.js
@@ -0,0 +1,1 @@
+(()=>{const o=window.open;top!=window&&(window.open=(n,e,w)=>(n&&new URL(n).href.match("s.1688.com/youyuan/index.htm\\?tab=imageSearch")&&(e="_self"),console.log(22222,n),o(n,e,w)))})();
